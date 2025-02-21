e660c5a78e0eeeee8b7fec9f8bdf9217a8168db6
KhalidEEE <82757724+KhalidEEE@users.noreply.github.com>
2025-02-21 14:44:56 +0300

    upd

--- test.py
+++ test.py
@@ -8,19 +8,19 @@ options_path = "/etc/bind/options.conf"
 with fileinput.FileInput(options_path, inplace=True) as file:
     for line in file:
         if file.lineno() == 16:
-            new_line = "listen-on { 127.0.0.1; 192.168.11.66; };"
+            new_line = "listen-on { 127.0.0.1; 192.168.11.66; };\n"
             print(new_line, end="")
         elif file.lineno() == 17:
-            new_line = "listen-on-v6 { any; };"
+            new_line = "listen-on-v6 { any; };\n"
             print(new_line, end="")
         elif file.lineno() == 24:
-            new_line = "forwarders { 77.88.8.8; };"
+            new_line = "forwarders { 77.88.8.8; };\n"
             print(new_line, end="")
         elif file.lineno() == 29:
-            new_line = "allow-query { 192.168.11.0/24; 192.168.33.0/24; };"
+            new_line = "allow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
             print(new_line, end="")
         elif file.lineno() == 30:
-            new_line = "allow-transfer { 192.168.33.66; };"
+            new_line = "allow-transfer { 192.168.33.66; };\n"
             print(new_line, end="")
         else:
             print(line, end="")
