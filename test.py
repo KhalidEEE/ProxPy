ec1da9c2da2a9cf2b232edc0c9b1f4a57c912b79
KhalidEEE <82757724+KhalidEEE@users.noreply.github.com>
2025-02-21 15:01:41 +0300

    upd

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ options_path = "/etc/bind/options.conf"
 with fileinput.FileInput(options_path, inplace=True) as file:
     for line in file:
         if file.lineno() == 16:
-            new_line = "\t\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
+            new_line = "\tlisten-on { 127.0.0.1; 192.168.11.66; };\n"
             print(new_line, end="")
         elif file.lineno() == 17:
-            new_line = "\t\tlisten-on-v6 { any; };\n"
+            new_line = "\tlisten-on-v6 { any; };\n"
             print(new_line, end="")
         elif file.lineno() == 24:
-            new_line = "\t\tforwarders { 77.88.8.8; };\n"
+            new_line = "\tforwarders { 77.88.8.8; };\n"
             print(new_line, end="")
         elif file.lineno() == 29:
-            new_line = "\t\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
+            new_line = "\tallow-query { 192.168.11.0/24; 192.168.33.0/24; };\n"
             print(new_line, end="")
         elif file.lineno() == 30:
-            new_line = "\t\t8allow-transfer { 192.168.33.66; };\n\n"
+            new_line = "\t8allow-transfer { 192.168.33.66; };\n\n"
             print(new_line, end="")
         else:
             print(line, end="")
